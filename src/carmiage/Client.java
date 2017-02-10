1a3137e9dbe789d85cc7ac67096f8ef2f1c70fb5
Deleplanque Dylan <deleplanque@a10p18.fil.univ-lille1.fr>
2017-02-10 11:05:02 +0100

    carmiage

--- src/carmiage/Client.java
+++ src/carmiage/Client.java
@@ -1,16 +1,19 @@
 package carmiage;
+
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.file.Files;
 import java.util.Scanner;
 
-
 public class Client {
-	private static Socket conn(){
+	private static Socket conn() {
 		Socket as = null;
 		try {
 			as = new Socket(InetAddress.getLocalHost(), 9632);
@@ -21,47 +24,40 @@ public class Client {
 		}
 		return as;
 	}
-	
-	public static void main(String [] args){
+
+	public static void main(String[] args) {
 		Socket as = null;
 		try {
 			while (true) {
 				Scanner scanner = new Scanner(System.in);
 				String sentence = "";
-	            System.out.println("\nCalculette\n");
-	            System.out.println("Avez-vous changer l'ip du serveur ?");
-	            System.out.println("(1) - Rentrer un calcul");
-	            System.out.println("(2) - Quitter");
+				System.out.println("Entrer un fichier");
 
-	            System.out.print("Rentrer le chiffre qui correspond à ce que vous souhaitez faire:\n");
-	            int selection = 0;
-	            try{
-	            	selection = scanner.nextInt();
-	            }catch(Exception e){
-	            	
-	            }
-	            if (selection == 1) {
-	            	as = conn();
-					DataOutputStream out = new DataOutputStream(as.getOutputStream());
-					BufferedReader in = new BufferedReader(
-					new InputStreamReader(as.getInputStream()));
-					
-					scanner = new Scanner(System.in);
-	                System.out.print("Entrer une opération: (op) (chiffre) (chiffre) \n");
-	                sentence = scanner.nextLine() + "\n";
+				String selection;
+				try {
+					selection = scanner.next();
+				} catch (Exception e) {
 
-	                out.writeBytes(sentence);
-	    			//System.out.println("Message envoyé");
-	    			String response = in.readLine();
-	    			System.out.println("Résultat obtenu: " + response);
-	    			
-		            as.close();
-	            }
-	            else if (selection == 2) {
-	                break;
-	            }
-	        }
-			
+				}
+
+				as = conn();
+				DataOutputStream out = new DataOutputStream(
+						as.getOutputStream());
+				BufferedReader in = new BufferedReader(new InputStreamReader(
+						as.getInputStream()));
+
+				scanner = new Scanner(System.in);
+				envoiFichier(as, scanner);
+
+				out.writeBytes(sentence);
+				// System.out.println("Message envoyé");
+				String response = in.readLine();
+				System.out.println("Résultat obtenu: " + response);
+
+				as.close();
+
+			}
+
 		} catch (UnknownHostException ex) {
 			System.out.println("Ip du serveur non trouvée");
 			System.exit(-1);
@@ -69,7 +65,17 @@ public class Client {
 			System.exit(-1);
 		}
 	}
+
+	public static void envoiFichier(Socket s, File f) {
+		try {
+			ObjectOutputStream out = new ObjectOutputStream(s.getOutputStream());
+			byte content[] = Files.readAllBytes(f.toPath());
+			out.writeObject(content);
+		} catch (Exception e) {
+			System.out
+					.println("EnvoiFichier : Erreur lors de l'envoi du fichier "
+							+ e);
+		}
+	}
+
 }
-
-
-
